f52eb5ca6ce27cd8e2688f46a2c3aee5e28df4af
destowq@gmail.com <destowq@gmail.com>
2024-11-02 20:38:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      welcome:"Welcome to Fullstack Development - I",
+      courseInfo:"React JS Programming Week 9 Lab Execerse",
+      name: "Daria Ignateva",
+      id: "101431121",
+      college:"George Brown College, Toronto"
+    }
+  }
+  
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{this.state.welcome}</h1>
+        <h2>{this.state.courseInfo}</h2>
+        <h3>{this.state.id}</h3>
+        <h4>{this.state.name}</h4>
+        <h5>{this.state.college}</h5>
     </div>
   );
+  }
 }
 
 export default App;
